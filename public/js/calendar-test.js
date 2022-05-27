f565ce170a66e0412db3e1a606b2cf9c63adacd0
Wataru <wataru2265@gmail.com>
2022-05-28 02:41:55 +0900

    calendar js css設定

--- public/js/calendar-test.js
+++ public/js/calendar-test.js
@@ -4,71 +4,89 @@ var __webpack_exports__ = {};
   !*** ./resources/js/calendar-test.js ***!
   \***************************************/
 // const {
-//     WeekNumberRoot
+//     WEEKNumberRoot
 // } = require("fullcalendar");
 // const {
 //     functionsIn
 // } = require("lodash");
 var WEEK = ['Sun', 'Mon', ' Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-var TODAY = new Date();
-var showDate = new Date(TODAY.getFullYear(), TODAY.getMonth(), 1);
+var TODAY = new Date(); // 月末だとずれる可能性があるため、1日固定で取得
+
+var showDate = new Date(TODAY.getFullYear(), TODAY.getMonth(), 1); // 初期表示
 
 window.onload = function () {
   showProcess(TODAY, calendar);
-};
+}; // 前の月表示
+
 
 function prev() {
   showDate.setMonth(showDate.getMonth() - 1);
   showProcess(showDate);
-}
+} // 次の月表示
+
 
 function next() {
   showDate.setMonth(showDate.getMonth() + 1);
   showProcess(showDate);
-}
+} // カレンダー表示
+
 
 function showProcess(date) {
   var year = date.getFullYear();
   var month = date.getMonth();
-  document.querySelector('#header').innerHTML = year + '年' + (month + 1) + '月';
+  document.querySelector('#header').innerHTML = year + "年 " + (month + 1) + "月";
   var calendar = createProcess(year, month);
   document.querySelector('#calendar').innerHTML = calendar;
-}
+} // カレンダー作成
+
 
 function createProcess(year, month) {
-  var calendar = '<table><tr class="dayOfWeek">';
+  // 曜日
+  var calendar = "<table><tr class='dayOfWeek'>";
 
   for (var i = 0; i < WEEK.length; i++) {
-    calendar += '<th>' + WEEK[i] + '</th>';
+    calendar += "<th>" + WEEK[i] + "</th>";
   }
 
-  calendar += '</tr>';
+  calendar += "</tr>";
   var count = 0;
-  var startDayOfWeek = new Date(year, month, 1).getDate();
+  var startDayOfWeek = new Date(year, month, 1).getDay();
   var endDate = new Date(year, month + 1, 0).getDate();
-  var row = Math.ceil((startDayOfWeek + endDate) / WEEK.length);
+  var lastMonthEndDate = new Date(year, month, 0).getDate();
+  var row = Math.ceil((startDayOfWeek + endDate) / WEEK.length); // 1行ずつ設定
 
   for (var _i = 0; _i < row; _i++) {
-    calendar += '<tr>';
+    calendar += "<tr>"; // 1colum単位で設定
 
-    for (var j; j < WEEK.length; j++) {
-      if (_i === 0 && j < startDayOfWeek) {
-        calendar += '<td class="disabled">' + (lastMonthEndDate - startDayOfWeek + j + 1) + '</td>';
+    for (var j = 0; j < WEEK.length; j++) {
+      if (_i == 0 && j < startDayOfWeek) {
+        // 1行目で1日まで先月の日付を設定
+        calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
       } else if (count >= endDate) {
+        // 最終行で最終日以降、翌月の日付を設定
+        count++;
+        calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
+      } else {
+        // 当月の日付を曜日に照らし合わせて設定
         count++;
 
-        if (year === TODAY.getFullYear() && month === TODAY.getMonth() && count === TODAY.getDate()) {
-          calendar += '<td class="today">' + count + '</td>';
+        if (year == TODAY.getFullYear() && month == TODAY.getMonth() && count == TODAY.getDate()) {
+          calendar += "<td class='TODAY'>" + count + "</td>";
         } else {
-          calendar += '<td>' + count + '</td>';
+          calendar += "<td>" + count + "</td>";
         }
       }
     }
 
-    calendar += '</tr>';
+    calendar += "</tr>";
   }
 
   return calendar;
 }
+
+var prevBtn = document.querySelector('#prev');
+var nextBtn = document.querySelector('#next');
+prevBtn.addEventListener('click', prev, false);
+nextBtn.addEventListener('click', next, false);
 /******/ })()
 ;
--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -1,14 +1,33 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            index
+            {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                {{-- <x-jet-welcome /> --}}
+
+                <div class="wrapper">
+                    <!-- xxxx年xx月を表示 -->
+                    <h3 id="header"></h3>
+
+                    <!-- ボタンクリックで月移動 -->
+                    <div id="next-prev-button">
+                        <button id="prev">‹</button>
+                        <button id="next">›</button>
+                    </div>
+
+                    <!-- カレンダー -->
+                    <div id="calendar"></div>
+                </div>
+
+                <hr>
+
+
+                <script src="{{ mix('js/calendar-test.js') }}"></script>
             </div>
         </div>
     </div>
