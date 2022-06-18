e7868976b8323522ca0baf982bfa02fd291af21d
Wataru <wataru2265@gmail.com>
2022-06-18 10:38:36 +0900

    holidays db 作成

---
+++ database/migrations/2022_06_18_103311_create_holidays_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('day_off');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('holidays');
+    }
+};
